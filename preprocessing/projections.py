049a3be9716f47e3ce724dfbd428bbf7d408e35b
mmarinated <marina.a.zavalina@gmail.com>
2020-04-25 00:52:28 -0400

    added huge module to compute labeled data

--- preprocessing/projections.py
+++ preprocessing/projections.py
@@ -1,222 +1,26 @@
-"""
-utility functions to project from 2d plane to another
-"""
-
+import matplotlib.cm as cm
 import numpy as np
-from ssl_project.utils import to_np
+from joblib import Parallel, delayed
 from sklearn.linear_model import LinearRegression
-from scipy.spatial import Delaunay
-from ssl_project.preprocessing import projections
-from paths import PATH_TO_REPO
+from tqdm import tqdm
 
-from constants import IDX_TO_ANGLE, PHOTO_H, PHOTO_W, VIEW_ANGLE
+import cv2
+from ssl_project.constants import *
+from ssl_project.paths import *
+from ssl_project.preprocessing import top_down_segmentation
 
-
-class PixelLabels:
-    def __init__(self, camera_height=1.7, alpha=VIEW_ANGLE, x_shift=0, *, 
-                 image_dims=(PHOTO_W, PHOTO_H)):
-        self.camera_height = camera_height
-        self.alpha = alpha
-        self.x_shift = x_shift
-        self.n_w, self.n_h = image_dims
-        x, y = np.meshgrid(np.arange(self.n_w), np.arange(self.n_h))
-        self.xy_N2 = np.vstack((x.ravel(), y.ravel())).T
-
-    @staticmethod
-    def _get_cars_n(target):
-        cars_n = np.array([
-            to_np(target["bounding_box"][car_idx].T)
-            for car_idx in range(len(target["bounding_box"]))
-        ])
-        
-        return cars_n
-
-    def get_pixel_segmentation_for_photos(self, target):
-        """
-        - assumes all cars are of height 1.5 m
-        - no roads
-        - no types
-
-        Returns
-        -------
-        mask_6hw
-        """
-        cars_n = self._get_cars_n(target)
-
-        target_6hw = np.zeros((6, self.n_h, self.n_w))
-
-        for photo_idx in range(6):
-            proj_plane = projections.ProjectionPlane(
-                self.camera_height, self.alpha, self.x_shift, 
-                IDX_TO_ANGLE[photo_idx])
-            valid_n = proj_plane.is_valid(cars_n)
-
-            for yz_42, yz_top_42 in zip(proj_plane(cars_n[valid_n]), 
-                                        proj_plane(cars_n[valid_n], 1.5)):
-                points = np.vstack((yz_42, yz_top_42))
-                hull = Delaunay(points)
-                mask_hw = (hull.find_simplex(self.xy_N2) >= 0).reshape(self.n_h, self.n_w)
-                target_6hw[photo_idx][mask_hw] = 1
-        
-        return target_6hw
+VALUE_TO_CATEGORY = top_down_segmentation.VALUE_TO_CATEGORY
+GROUND_CAT = 13
+assert VALUE_TO_CATEGORY[GROUND_CAT] == 'ground'
 
 
-class ProjectionPlane:
-    """
-    # image # 57 in labeled_dataset
-    >>> proj_plane = ProjectionPlane(1.7, 70, IDX_TO_ANGLE[1])
-    >>> cars_n  = np.load(f"{PATH_TO_REPO}/tests/cars.npy")
-    >>> valid_n = proj_plane.is_valid(cars_n)
-    >>> yz_k42  = proj_plane(cars_n[valid_n])
-    >>> yz_k42[0]
-    array([[ 54.38444167, 139.41474844],
-           [ 48.16907255, 140.11217843],
-           [ 25.32221255, 139.39299565],
-           [ 17.34732646, 140.08768914]])
-    >>> valid_n.size, valid_n.sum()
-    (21, 5)
-    """
-    
-    def __init__(self, camera_height, alpha, x_shift=0, rotation_angle=0, *, 
-                 image_dims=(PHOTO_W, PHOTO_H)):
-        self.camera_height  = camera_height
-        self.angle_view     = np.radians(alpha)
-        self.x_shift        = x_shift
-        self.rotation_angle = np.radians(rotation_angle)
-        self.image_width, self.image_height = image_dims
-    
-        self._init_projection_plane()
-        self._init_borders()
-        self._init_scaler()
+NO_IMAGE_LABEL = -1
 
-    def _init_projection_plane(self):
-        self._view_point = (self.x_shift, 0, self.camera_height) # maybe TODO 
-    
-        # TODO based on rotation alpha
-        x, y = np.cos(self.rotation_angle), np.sin(self.rotation_angle)
-        self._norm_vector_to_plane      = (x, y, 0) # should not add x_shift
-        self._point_in_projection_plane = (x + self.x_shift, y, self.camera_height)
-        assert (0.9999 < x**2 + y**2 < 1.0001) # dist to projection plane
-
-    def _init_borders(self):
-        """
-        Assumes (0, 0) is the center
-        """
-        # TODO
-        
-        self._border_w = np.tan(self.angle_view / 2) # multiply by dist to projection plane, which is 1
-        self._border_h = self.image_height / self.image_width * self._border_w
-
-        # TODO REMOVE
-        self.borders_42 = np.array([
-            [self._border_w,  self.camera_height + self._border_h],
-            [self._border_w,  self.camera_height - self._border_h],
-            [-self._border_w, self.camera_height + self._border_h],
-            [-self._border_w, self.camera_height - self._border_h]
-        ])
+__all__ = ['create_label_data', 'create_data_all']
 
 
-    def _init_scaler(self):
-        def get_linear_tansform(x1, x2, new_x1, new_x2):
-            lr = LinearRegression()
-            lr.fit([[x1], [x2]], [new_x1, new_x2])
-            return lr
-
-        self._x_axis_transform = get_linear_tansform(
-            -self._border_w, self._border_w,
-            0, self.image_width)
-        self._y_axis_transform = get_linear_tansform(
-            self.camera_height - self._border_h, 
-            self.camera_height + self._border_h, 
-            0, self.image_height)
-
-
-    def __call__(self, xy_n42, height=0.):
-        """
-        Return
-        ------
-        yz_n42
-        """
-        yz_n42 = np.zeros_like(xy_n42)
-        for n_idx, xy_42 in enumerate(xy_n42):
-            yz_n42[n_idx] = self._get_bounding_box_helper(xy_42, height)
-        return yz_n42
-
-
-    def _get_bounding_box_helper(self, xy_42, height):
-        yz_42 = np.zeros((4, 2)) # (y, z)
-        func = projection_func(
-            np.array(self._view_point),
-            np.array(self._point_in_projection_plane), 
-            np.array(self._norm_vector_to_plane))
-
-        for idx, (x_topdown, y_topdown) in enumerate(xy_42):
-            rot_x, rot_y, yz_42[idx][1] = func(np.array([x_topdown, y_topdown, height])[None])[0]
-            
-            rot_x -= self.x_shift
-            x, yz_42[idx][0] = _rotate((rot_x, rot_y), -self.rotation_angle)
-            assert 0.9999 < x < 1.0001
-
-        yz_42 = self._scale(yz_42)
-        return yz_42
-
-    def _scale(self, yz_42):
-        new_yz_42 = yz_42.copy()
-        # NOTE: flip width and height to match the photo
-        new_yz_42[..., 0] = self.image_width - self._x_axis_transform.predict(yz_42[..., 0:1])
-        new_yz_42[..., 1] = self.image_height - self._y_axis_transform.predict(yz_42[..., 1:2])
-
-        return new_yz_42
-
-
-    def is_valid(self, xy_n42, height=0):
-        """checks if ALL corners are visible and front"""
-        return np.array([self._is_valid_helper(xy_42, height) for xy_42 in xy_n42])
-
-    def _is_valid_helper(self, xy_42, height):
-        is_valid = self._is_front(*xy_42[0])
-        yz_42 = self(xy_42[None], height)[0]
-
-        for (y, z) in yz_42:
-             is_valid &= self._is_visible(y, z)   
-
-
-        # at_least_one_visible = False
-        # for (y, z) in yz_42:
-        #      at_least_one_visible |= self._is_visible(y, z)   
-
-        # is_valid &= at_least_one_visible
-
-        return is_valid
-
-    def _is_visible(self, y, z):
-        return (0 < y < self.image_width) and (0 < z < self.image_height)
-
-    def _is_front(self, x, y):
-        dist_1  = x**2 + y**2
-        dist_2 = ((self._point_in_projection_plane[0] - x)**2
-                + (self._point_in_projection_plane[1] - y)**2)
-
-        return dist_1 > dist_2
-
-
-import math
-
-def _rotate(point, angle):
-    """
-    Rotate a point counterclockwise by a given angle around a given origin.
-
-    The angle should be given in radians.
-    """
-    ox, oy = (0, 0)
-    px, py = point
-
-    qx = ox + math.cos(angle) * (px - ox) - math.sin(angle) * (py - oy)
-    qy = oy + math.sin(angle) * (px - ox) + math.cos(angle) * (py - oy)
-    return qx, qy
-
-
-class projection_func:
+class projection_func:  
+    """used to get corners etc, for images we use cv2"""
     def __init__(self, view_3, in_plane_3, orth_3):
         self.view_3 = view_3
         self.in_plane_3 = in_plane_3
@@ -230,3 +34,216 @@ class projection_func:
         diff_n3  *= (self.coef / dot_n)[:, None]
 
         return self.view_3 + diff_n3
+
+
+
+def rotate_image(image, angle, borderValue):
+    """rotates with padding"""
+    image_center = tuple(np.array(image.shape[1::-1]) / 2)
+    rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)
+    result  = cv2.warpAffine(image, rot_mat, image.shape[1::-1], 
+                             flags=cv2.INTER_NEAREST, borderValue=borderValue)
+    return result
+
+
+class Corners:
+    def __init__(self, camera_height_in_m, alpha, level_height_in_m=0, *,
+                 image_dims=(PHOTO_W, PHOTO_H)):
+        """
+        Assumes dist to projection plane is 1.
+        
+        height = horizontal plane (e.g. road) height
+        """
+        assert level_height_in_m < camera_height_in_m
+        
+        self.camera_height  = camera_height_in_m * 10
+        self.angle_view     = np.radians(alpha)
+        self.level_height   = level_height_in_m * 10
+        
+        self.image_width, self.image_height = image_dims
+        
+        self.view_point = np.array([0, 0, self.camera_height])
+        
+        self._f_xyz_to_camera_plane = projection_func(
+            self.view_point,
+            np.array([1, 0, 0,]),
+            np.array([1, 0, 0,]),
+        )
+
+        self._f_camera_to_xy_h = projection_func(
+            self.view_point,
+            np.array([0, 0, self.level_height]),
+            np.array([0, 0, 1,]),
+        )
+    
+
+    def _scale(self, yz_42):
+        new_yz_42 = yz_42.copy()
+        new_yz_42[..., 0] = self._y_axis_transform.predict(yz_42[..., 0:1])
+        new_yz_42[..., 1] = self._z_axis_transform.predict(yz_42[..., 1:2])
+
+        return new_yz_42
+
+
+    def get_photo_and_map_corners(self):
+        """Returns photo_corners_43, map_corners_43"""
+        photo_down_corners_23 = self._get_photo_down_corners()
+        map_back_corners_23  = self._get_map_back_corners()
+        photo_up_corners_23  = self._get_photo_up_corners(map_back_corners_23)
+        map_front_corners_23 = self._get_map_front_corners(photo_down_corners_23)
+        
+        tmp_photo_corners_43, tmp_map_corners_43 = (
+            np.vstack([photo_down_corners_23, photo_up_corners_23]), 
+            np.vstack([map_front_corners_23, map_back_corners_23]),
+        )
+        
+        photo_corners_yz_42 = self._scale(tmp_photo_corners_43[:, 1:])
+        map_corners_xy_42 = tmp_map_corners_43[:, :2] + 400
+        return photo_corners_yz_42.astype(int), map_corners_xy_42.astype(int)
+
+    def _get_photo_down_corners(self, dist_to_plane=1):
+        y_diff = np.tan(self.angle_view / 2) * dist_to_plane
+        z_diff = y_diff * self.image_height / self.image_width
+    
+        def get_linear_tansform(x1, x2, new_x1, new_x2):
+            lr = LinearRegression()
+            lr.fit([[x1], [x2]], [new_x1, new_x2])
+            return lr
+
+    
+        self._y_axis_transform = get_linear_tansform(
+            - y_diff, y_diff, 0, self.image_width)
+        self._z_axis_transform = get_linear_tansform(
+            self.camera_height + z_diff, 
+            self.camera_height - z_diff, 
+            0, self.image_height)
+    
+        return np.array([
+            [1, - y_diff, self.camera_height - z_diff],
+            [1, + y_diff, self.camera_height - z_diff],
+        ])
+
+    def _get_map_back_corners(self):
+        dist_far = 400 # dist from center to border
+        y_diff = np.tan(self.angle_view / 2) * dist_far
+        return np.array([
+            [dist_far, - y_diff, self.level_height],
+            [dist_far, + y_diff, self.level_height],
+        ])
+
+    def _get_photo_up_corners(self, map_back_corners_23):
+        return self._f_xyz_to_camera_plane(map_back_corners_23)
+
+    def _get_map_front_corners(self, photo_down_corners_23):
+        return self._f_camera_to_xy_h(photo_down_corners_23)
+
+
+def get_warped_hw(
+        segm_WW, height_WW, *, 
+        camera_idx, level_height_in_m,
+        camera_height_in_m = 1.7):
+    
+    segm_WW    = rotate_image(segm_WW, -IDX_TO_ANGLE[camera_idx],   borderValue=GROUND_CAT)
+    height_WW  = rotate_image(height_WW, -IDX_TO_ANGLE[camera_idx], borderValue=0)
+
+    
+
+    if level_height_in_m > camera_height_in_m:
+        level_height_in_m_fake = 2 * camera_height_in_m - level_height_in_m 
+        inverse = True
+    else:
+        level_height_in_m_fake = level_height_in_m
+        inverse = False
+    
+    corners = Corners(camera_height_in_m=camera_height_in_m, 
+                      alpha=VIEW_ANGLE, level_height_in_m=level_height_in_m_fake)
+    photo_corners_yz_42, map_corners_xy_42 = corners.get_photo_and_map_corners()
+
+    
+    points_src, points_tgt = map_corners_xy_42.astype("float32"), photo_corners_yz_42.astype("float32")
+    h_size = int(points_tgt[0, 1] - points_tgt[-1, 1])
+
+    M = cv2.getPerspectiveTransform(points_src, points_tgt)
+    
+    def get_warped_image(image_WW):
+        return cv2.warpPerspective(image_WW[:, ::-1], M, (PHOTO_W, h_size), flags=cv2.INTER_NEAREST).astype(int)
+    
+    warped_segm    = get_warped_image(segm_WW)
+    warped_height  = get_warped_image(height_WW)
+    
+    warped_segm[warped_height < level_height_in_m] = NO_IMAGE_LABEL
+    answer_hw = np.full((PHOTO_H, PHOTO_W), NO_IMAGE_LABEL)
+    answer_hw[PHOTO_H - h_size:] = warped_segm[::-1, ::-1]
+    
+    return answer_hw[::-1] if inverse else answer_hw
+
+
+
+def create_height_WW(segm_WW):
+    height_WW = np.zeros_like(segm_WW)
+    for val, category in top_down_segmentation.VALUE_TO_CATEGORY.items():
+        if category in CATEGORY_TO_HEIGHT:
+            height = CATEGORY_TO_HEIGHT[category]
+            height_WW[segm_WW == val] = height
+
+    return height_WW
+
+
+def create_dist_WW():
+    dist_WW = np.zeros((800, 800))
+    dist_WW += ((np.arange(800) - 400)**2)[None]
+    dist_WW += ((np.arange(800) - 400)**2)[:, None]
+    dist_WW /= 100
+    dist_WW = np.sqrt(dist_WW)
+    return dist_WW
+
+
+def create_segm_hw(top_down_segm_WW, height_WW, camera_idx):
+    segm_hw     = get_warped_hw(top_down_segm_WW, height_WW, 
+                                camera_idx=camera_idx, level_height_in_m=0)
+
+    for height in np.arange(0, height_WW.max(), 0.05): # TODO -- main speed up should be here
+        tmp_hw     = get_warped_hw(top_down_segm_WW, height_WW,  
+                                    camera_idx=camera_idx, level_height_in_m=height)
+        np.copyto(segm_hw, tmp_hw, where=(tmp_hw != NO_IMAGE_LABEL))
+
+    # HACK HERE
+    # idea is that we have empty spots near 1.7 let's fill it dirty way 
+    for height in np.arange(1.6, min(height_WW.max(), 1.8), 0.025): # TODO -- main speed up should be here
+        tmp_hw     = get_warped_hw(top_down_segm_WW, height_WW,  
+                                    camera_idx=camera_idx, level_height_in_m=height,
+                                    camera_height_in_m=2)
+        np.copyto(segm_hw, tmp_hw, where=(tmp_hw != NO_IMAGE_LABEL))
+
+
+    return segm_hw  
+
+
+def create_label_data(scene_id):
+    dist_WW = create_dist_WW()  
+
+    
+    for sample_id in tqdm(range(NUM_SAMPLE_PER_SCENE)):
+        PATH_TMP = f"{PATH_TO_DATA}/scene_{scene_id}/sample_{sample_id}"
+        top_down_segm_WW = cv2.imread(f"{PATH_TMP}/top_down_segm.png", cv2.IMREAD_UNCHANGED)
+        height_WW = create_height_WW(top_down_segm_WW)
+        
+        for idx, camera_name in enumerate(CAM_NAMES):
+            # photo_hw = cv2.imread(f"../data/scene_{scene_id}/sample_{sample_id}/{name}.jpeg", cv2.IMREAD_UNCHANGED)
+            segm_hw = create_segm_hw(top_down_segm_WW, height_WW, idx)
+            segm_hw[segm_hw == NO_IMAGE_LABEL] = max(top_down_segmentation.VALUE_TO_CATEGORY.keys()) + 1
+            dist_hw = create_segm_hw(dist_WW,          height_WW, idx)
+
+            cv2.imwrite(f"{PATH_TMP}/SEGM_{camera_name}.png", segm_hw)
+            cv2.imwrite(f"{PATH_TMP}/DIST_{camera_name}.png", dist_hw)
+
+
+def create_data_all(n_jobs=8, slc=slice(None), debug=False):    
+    if debug:
+        for idx in tqdm(LABELED_SCENE_INDEX[slc]):
+            create_label_data(idx)
+    else:
+        Parallel(n_jobs=n_jobs)(
+            delayed(create_label_data)(scene_id) 
+            for scene_id in LABELED_SCENE_INDEX[slc]
+        )
